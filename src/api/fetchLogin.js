0672012def9e2e05e34569e5dd068ca80e491322
dongbinkoo <rnehdqlsdbxbqm@gmail.com>
2025-02-15 17:04:39 +0900

    백엔드 서버주소 변경

--- src/api/fetchLogin.js
+++ src/api/fetchLogin.js
@@ -1,6 +1,6 @@
 async function fetchLogin(email, password) {
     try {
-        const response = await fetch('https:///auth/login', {
+        const response = await fetch('https://interviewai-ij1p.onrender.com/auth/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
