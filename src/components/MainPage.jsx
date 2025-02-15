5ee7534198eba738297707dc07ab5360badb1121
dongbinkoo <rnehdqlsdbxbqm@gmail.com>
2025-02-15 16:46:06 +0900

    백엔드 서버주소 변경

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -6,7 +6,7 @@ import MainVideo from './MainVideo';
 
 async function fetchRegister(name, email, password) {
     try {
-        const response = await fetch('https://interviewai-ij1p.onrender.com/auth/register', {
+        const response = await fetch('https://interview-c4s12l05c-dongbinkoos-projects.vercel.app/auth/register', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -30,7 +30,7 @@ async function fetchRegister(name, email, password) {
 
 async function fetchQuestions(job) {
     try {
-        const response = await fetch('https://interviewai-ij1p.onrender.com/api/generate-questions', {
+        const response = await fetch('https://interview-c4s12l05c-dongbinkoos-projects.vercel.app/api/generate-questions', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
