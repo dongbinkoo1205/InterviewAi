24cf537f7d7fd081618fb964a8e996c2f12928e3
dongbinkoo <rnehdqlsdbxbqm@gmail.com>
2025-02-15 14:39:34 +0900

    railway 추가

--- backend/route/login.js
+++ backend/route/login.js
@@ -9,6 +9,7 @@ const mysql = require('mysql2/promise');
 // ✅ MySQL 데이터베이스 연결 설정
 const db = mysql.createPool({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
